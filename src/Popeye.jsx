74035c7fbbc1359e895d7d8e9f6884048a7901ec
AmeerSyed7340 <Ameer.Syed40@qmail.cuny.edu>
2023-08-30 16:56:08 -0400

    Initial Commit

---
+++ src/Popeye.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+const Popeye = () => {
+    return (
+        <>
+        <p>Hi, This is Popeye</p>
+        <Link to="/">Click here to go back</Link>
+        </>
+    )
+}
+
+export default Popeye;
