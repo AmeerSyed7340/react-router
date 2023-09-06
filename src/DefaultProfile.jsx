cab52445c50c25e7de8a5c431dea6cbfa0e9003a
AmeerSyed7340 <Ameer.Syed40@qmail.cuny.edu>
2023-09-06 19:13:20 -0400

    Extracted router configuration into its own component

---
+++ src/DefaultProfile.jsx
@@ -0,0 +1,7 @@
+const DefaultProfile = () => {
+    return <p>Oh, nothing to see here!</p>;
+
+};
+
+
+export default DefaultProfile;
--- src/Profile.jsx
+++ src/Profile.jsx
@@ -1,15 +1,24 @@
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, Link, useParams, useResolvedPath } from "react-router-dom";
+import Popeye from "./Popeye";
+import Spinach from "./Spinach";
+import DefaultProfile from "./DefaultProfile";
 const Profile = () => {
+    const { name } = useParams();
     return (
         <div>
             <h1>Hello from profile page</h1>
             <p>So, how are you?</p>
             <hr />
             <h2>The profile visited is here:</h2>
-            <Link to="spinach">Spinach</Link>
+            {/* <Link to="spinach">Spinach</Link>
             <br />
             <Link to="popeye">Popeye</Link>
-            <Outlet />
+            <Outlet /> */}
+
+            {name === "popeye" ? (<Popeye />)
+                : name === "spinach" ? (<Spinach />)
+                    : (<DefaultProfile />)
+            }
         </div>
     )
 }
